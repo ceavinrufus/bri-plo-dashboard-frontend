89852304be3bfa4834582e8ddaa8b041e48ef4e8
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-10-08 13:40:41 +0700

    feat: filter

--- src/components/DashboardMetrics.jsx
+++ src/components/DashboardMetrics.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
-import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardDescription,
-    CardTitle,
-} from '@/components/ui/card'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 const DashboardMetrics = ({ metrics }) => {
     return (
