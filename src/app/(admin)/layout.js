2e50633d94f1a58b960a1dd097ec66aec6cf25c2
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-11-28 11:41:49 +0700

    feat: add sidebar in admin layout

--- src/app/(admin)/layout.js
+++ src/app/(admin)/layout.js
@@ -1,7 +1,17 @@
+'use client'
+
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import { AppSidebar } from '@/components/app-sidebar'
+import { AppSidebar } from '@/components/admin/app-sidebar'
+import { useAuth } from '@/hooks/auth'
+import Loading from '../(app)/Loading'
 
 export default function Layout({ children }) {
+    const { user } = useAuth({ middleware: 'admin' })
+
+    if (!user) {
+        return <Loading />
+    }
+
     return (
         <SidebarProvider>
             <AppSidebar />
--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -1,9 +1,10 @@
-"use client";
+"use client"
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { cva } from "class-variance-authority";
+import { cva } from "class-variance-authority"
 import { useIsMobile } from "@/hooks/use-mobile"
-import { cn } from "@/components/lib/utils"
+import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
