3288ee9578a5173423706325c26a424fda8142f4
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-12-02 15:36:12 +0700

    fix: call can check all pengadaan data

--- src/app/(app)/proses-pengadaan/page.js
+++ src/app/(app)/proses-pengadaan/page.js
@@ -13,7 +13,7 @@ const ProsesPengadaan = () => {
             <div className="mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 bg-white border-b border-gray-200">
-                        {canCheckAllPengadaanData(user.role) ? (
+                        {canCheckAllPengadaanData(user) ? (
                             <Tabs
                                 orientation="vertical"
                                 defaultValue="all"
