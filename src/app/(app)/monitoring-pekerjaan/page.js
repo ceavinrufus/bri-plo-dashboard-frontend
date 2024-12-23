9a9fbe287cd0413e25d29793a1d645fe3e525b5a
ceavinrufus <18221162@std.stei.itb.ac.id>
2024-12-23 11:23:43 +0700

    coming soon

--- src/app/(app)/monitoring-pekerjaan/page.js
+++ src/app/(app)/monitoring-pekerjaan/page.js
@@ -4,7 +4,7 @@ const MonitoringPekerjaan = () => {
             <div className="mx-auto sm:px-6 lg:px-8">
                 <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div className="p-6 bg-white border-b border-gray-200">
-                        You are logged in!
+                        Coming soon!
                     </div>
                 </div>
             </div>
